a520692ed0af965b2dd97274547231a0c0326994
Dewen Xu <tracyx008@gmail.com>
2016-11-14 16:08:37 -0500

    ...

--- merge.R
+++ merge.R
@@ -1,12 +1,13 @@
+library(raster)
 library(dplyr)
 library(ggplot2)
 library(sf)
 library(stringr)
 library(magrittr)
-library(stringr)
 library(tibble)
 
 load("/data/nyc_parking/NYParkingViolations.Rdata")
+altnames <- read.csv("/data/nyc_parking/altnames.csv")
 
 ## Get pluto data
 
@@ -44,33 +45,78 @@ pluto_xy %<>% mutate(address = tolower(address))
 xy = pluto_xy %>% 
   filter(address != "")
 
-#regst = "(\\sstreet)"
+# Replace strings
+# find the most occurance 10 street type
+alt.data = read.csv(file="/data/nyc_parking/altnames.csv", na.strings=c("","NA"))
+alt.data.stype = alt.data %>% 
+  select(SType) %>% 
+  table() %>%
+  sort(, decreasing = TRUE) %>% 
+  head(,n = 10) %>% 
+  names()
+alt.data.st.rep = c("avenue", "street", "place","way","blvd","road","bridge","drive","court", "parkway")
+match.name = data.frame(tolower(alt.data.stype), alt.data.st.rep, stringsAsFactors = FALSE)
 
-xy$address=str_replace(xy$address, "street", "st")
-nyc_man$address = str_replace(nyc_man$address, "street", "st")
-xy$address=str_replace(xy$address, "avenue", "ave")
-nyc_man$address = str_replace(nyc_man$address, "avenue", "ave")
-xy$address=str_replace(xy$address, "street", "st")
-nyc_man$address = str_replace(nyc_man$address, "street", "st")
-xy$address=str_replace(xy$address, "boulevard", "blvd")
-nyc_man$address = str_replace(nyc_man$address, "boulevard", "blvd")
-xy$address=str_replace(xy$address, "drive", "dr")
-nyc_man$address = str_replace(nyc_man$address, "drive", "dr")
+# find the directions
+alt.data.dir = alt.data %>% select(PDir) %>% unique() %>% na.omit()
+alt.data.dir.rep = c("east", "west","south","north") 
+dir.name = data.frame(tolower(alt.data.dir[[1]]),alt.data.dir.rep,stringsAsFactors = FALSE) 
 
-nyc_man$address = str_replace(nyc_man$address, "first", "1")
-nyc_man$address = str_replace(nyc_man$address, "second", "2")
-nyc_man$address = str_replace(nyc_man$address, "third", "3")
-nyc_man$address = str_replace(nyc_man$address, "fourth", "4")
-nyc_man$address = str_replace(nyc_man$address, "fifth", "5")
+#find the street numbers
+num.rep = c(1:10)
+num = c("1st","2nd","3rd","4th","5th","6th","7th","8th","9th","10th")
 
-#detect 
-#head(nyc_man$address[str_detect(nyc_man$address, "(\\d+\\w*(st|nd|rd|th))")])
+# make the address format to be consistant through the nyc_man file
+for (i in seq_len(dim(match.name)[1])){
+  nyc_man$address = str_replace(nyc_man$address, paste0(match.name[i,1],"$"),match.name[i,2])
+}
+for ( k in seq_along(num.rep)){
+  nyc_man$address = str_replace(nyc_man$address, paste0(" ",num[k], " "),paste0(" ",num.rep[k], " "))
+}
+for ( j in seq_len(dim(dir.name)[1])){
+  nyc_man$address = str_replace(nyc_man$address, paste0(" ",dir.name[j,1], " "),paste0(" ",dir.name[j,2], " "))
+}
+# make the address format to be consistant through the pluto_xy file
+pluto_xy$address = str_replace(pluto_xy$address, "bl$", "blvd")
 
-combined = inner_join(nyc_man, xy)
-#notcombined = anti_join(nyc_man, pluto_xy)
+
+# Combine data
+combined = inner_join(nyc_man, pluto_xy)
 
 ggplot(combined, aes(x=x,y=y,color=factor(precinct))) + 
   geom_point(size=0.1) +
   theme_bw()
+
+save(combined, file="precinct.Rdata")
+
+#regst = "(\\sstreet)"
+#
+#xy$address=str_replace(xy$address, "street", "st")
+#nyc_man$address = str_replace(nyc_man$address, "street", "st")
+#xy$address=str_replace(xy$address, "avenue", "ave")
+#nyc_man$address = str_replace(nyc_man$address, "avenue", "ave")
+#xy$address=str_replace(xy$address, "street", "st")
+#nyc_man$address = str_replace(nyc_man$address, "street", "st")
+#xy$address=str_replace(xy$address, "boulevard", "blvd")
+#nyc_man$address = str_replace(nyc_man$address, "boulevard", "blvd")
+#xy$address=str_replace(xy$address, "drive", "dr")
+#nyc_man$address = str_replace(nyc_man$address, "drive", "dr")
+#
+#
+#nyc_man$address = str_replace(nyc_man$address, "first", "1")
+#nyc_man$address = str_replace(nyc_man$address, "second", "2")
+#nyc_man$address = str_replace(nyc_man$address, "third", "3")
+#nyc_man$address = str_replace(nyc_man$address, "fourth", "4")
+#nyc_man$address = str_replace(nyc_man$address, "fifth", "5")
+#
+##detect 
+##head(nyc_man$address[str_detect(nyc_man$address, "(\\d+\\w*(st|nd|rd|th))")])
+#
+#combined = inner_join(nyc_man, xy)
+##notcombined = anti_join(nyc_man, pluto_xy)
+#
+#ggplot(combined, aes(x=x,y=y,color=factor(precinct))) + 
+#  geom_point(size=0.1) +
+#  theme_bw()
 
 save(combined, file="precinct.Rdata")
