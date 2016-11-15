1cd8e7e88c250c8cef2bab5287f1e16e65a5fecb
Dewen Xu <tracyx008@gmail.com>
2016-11-15 12:28:21 -0500

    ,,,

--- df.R
+++ df.R
@@ -15,13 +15,8 @@ Csamp <- function(n,rad=1,centre=c(0,0)){
 samp1<-Csamp(1000,0.0065,c(-73.974309,40.770629))
 samp2<-Csamp(1000,0.0063,c(-73.968815,40.778104))
 samp3<-Csamp(1000,0.0063,c(-73.962721,40.785075))
-samp4<-Csamp(1000,0.0063,c(-73.959117,40.790858))
-<<<<<<< HEAD
-samp5<-Csamp(600,0.0063,c(-73.956284,40.795244))
-=======
-samp5<-Csamp(1000,0.0063,c(-73.955855,40.795244))
->>>>>>> cf6ff3d6ea02253c3d413170342293a8a585eabb
-
+samp4<-Csamp(1500,0.0065,c(-73.959117,40.790858))
+samp5<-Csamp(1500,0.0065,c(-73.956284,40.795244))
 
 sample_together <-data.frame(rbind(samp1,samp2,samp3,samp4,samp5))
-df <- data.frame(address = rep("Central Park",4600),precinct = 22,sample_together)
+df <- data.frame(address = rep("Central Park",6000),precinct = 22,sample_together)
