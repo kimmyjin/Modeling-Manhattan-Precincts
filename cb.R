f14f9881d26520803a9627558c7ca0d6c6824d99
Dewen Xu <tracyx008@gmail.com>
2016-11-17 00:00:36 -0500

    ...

--- cb.R
+++ cb.R
@@ -4,26 +4,23 @@ c2 = c(-73.957995, 40.800545)
 c3 = c(-73.973021, 40.764427)
 c4 = c(-73.949281, 40.796896)
 
-x = seq(c1[1],c2[1], length.out = 10)
-y = seq(c1[2],c2[2], length.out = 10)
+
+x = seq(c1[1],c2[1], length.out = 50)
+y = seq(c1[2],c2[2], length.out = 50)
 cbound1 = cbind(x, y)
-x = seq(c1[1],c2[1], length.out = 10)
-y = seq(c1[2],c2[2], length.out = 10)
-cbound12 = cbind(x, y)
 x = seq(c1[1],c2[1], length.out = 100)
 y = seq(c1[2],c2[2], length.out = 100)
 cbound2 = cbind(x, y)
 
-th = (c3-c1)/22
-theta1 = matrix(NA, nrow =10, ncol =2)
+th = (c3-c1)/20
+theta1 = matrix(NA, nrow =50, ncol =2)
 theta2 = matrix(NA, nrow =100, ncol =2)
-theta1[,1] = as.numeric(paste(rep(th[1],10)))
-theta1[,2] = as.numeric(paste(rep(th[2],10)))
+theta1[,1] = as.numeric(paste(rep(th[1],50)))
+theta1[,2] = as.numeric(paste(rep(th[2],50)))
 theta2[,1] = as.numeric(paste(rep(th[1],100)))
 theta2[,2] = as.numeric(paste(rep(th[2],100)))
 
-cbound12 = cbound12 + theta1[1:10,]
-cbound = rbind(cbound1, cbound12)
+cbound = cbound1 + theta1
 
 original2 = cbound2
 for (i in 2:17){
@@ -31,14 +28,15 @@ for (i in 2:17){
   cbound = rbind(cbound, temp)
 }
 
-x = seq(c3[1],c4[1], length.out = 300)
-y = seq(c3[2],c4[2], length.out = 300)
+x = seq(c3[1],c4[1], length.out = 200)
+y = seq(c3[2],c4[2], length.out = 200)
 cbound3 = cbind(x, y)
-theta3 = matrix(NA, nrow =300, ncol =2)
-theta3[,1] = as.numeric(paste(rep(th[1],300)))
-theta3[,2] = as.numeric(paste(rep(th[2],300)))
+theta3 = matrix(NA, nrow = 200, ncol = 2)
+theta3[,1] = as.numeric(paste(rep(th[1],200)))
+theta3[,2] = as.numeric(paste(rep(th[2],200)))
 
 nextbound = cbound3 + (-theta3)
 cbound = rbind(cbound,nextbound, cbound3)
 
-cb <-data.frame(address = rep("Central Park",2220),precinct = 22,cbound)
+
+cb <-data.frame(address = rep("Central Park",2050),precinct = 22,cbound)
